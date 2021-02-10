7ea1b2ef948c8a772c8ec99329707c6f9d09b8d5
Heiko Stowasser <stowasserheiko@gmail.com>
2021-02-10 17:38:06 -0500

    Implemented line count in file reader

--- include/FileReader.hpp
+++ include/FileReader.hpp
@@ -1,12 +1,20 @@
 #pragma once
 
 #include <string>
+#include <iostream>
+#include <fstream>
 
 class FileReader
 {
 private:
-    /* data */
+    std::ifstream *fin_;
+    unsigned int linenum_;
 public:
     FileReader(std::string filename);
     ~FileReader();
+
+    char peekc(); // Get next char without moving pointer
+    char getc(); // Get next char
+    unsigned int getLineNum();
+    bool eof();
 };
--- src/FileReader.cpp
+++ src/FileReader.cpp
@@ -2,10 +2,43 @@
 #include <iostream>
 #include <fstream>
 
+using namespace std;
+
 FileReader::FileReader(std::string filename)
 {
+    fin_ = new std::ifstream();
+    fin_->open(filename, std::ios::in);
 }
 
 FileReader::~FileReader()
 {
+    fin_->close();
+    free(fin_);
+}
+
+unsigned int FileReader::getLineNum() 
+{
+    return linenum_;
+}
+
+char FileReader::peekc()
+{
+    char c;
+    c = fin_->peek();
+    return c;
+}
+
+char FileReader::getc()
+{
+    char c;
+    fin_->get(c);
+    if (c == '\n'){
+        linenum_++;
+    }
+    return c;
+}
+
+bool FileReader::eof()
+{
+    return fin_->eof();
 }
